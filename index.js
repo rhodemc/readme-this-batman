a2af2a13ca8a93d5b87d951e566d7ade1e691d78
rhodemc <rhodemc@gmail.com>
2023-03-26 15:04:23 -0700

    Working README generator plus README files

--- index.js
+++ index.js
@@ -1,18 +1,83 @@
-// TODO: Include packages needed for this application
+// Packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
 
-const inquirer = require('inquirer');
-const fs = require('fs');
+// Link to README markdown creation
+const generateMarkdown = require("./utils/generateMarkdown");
 
-const generateReadme = readmeplaceholder;
+// Array of questions for user input
+const questions = () => {
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "github",
+        message: "What is your GitHub username?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is your email address?",
+      },
+      {
+        type: "input",
+        name: "title",
+        message: "What is the name of your project?",
+      },
+      {
+        type: "input",
+        name: "description",
+        message: "Please write a short description of your project:",
+      },
+      {
+        type: "list",
+        name: "licenseChoice",
+        message: "What kind of license does your project have?",
+        choices: ["MIT", "MPL-2.0", "None"],
+      },
+      {
+        type: "input",
+        name: "installation",
+        message: "Which command should be used to install dependencies?",
+      },
+      {
+        type: "input",
+        name: "tests",
+        message: "Which command should be used to run tests?",
+      },
+      {
+        type: "input",
+        name: "usage",
+        message: "What should the user know about running the application?",
+      },
+      {
+        type: "input",
+        name: "contributions",
+        message: "Were there any outside contributions to the project?",
+      },
+    ])
+};
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Function to write README file
+const writeFile = (userInput) => {
+  fs.writeFile("README.md", userInput, (err) => {
+    err ? console.log(err) : console.log("Successfully created your README file!");
+  });
+};
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
+// Function to initialize app
+function init() {
+  questions()
+    .then((answers) => {
+      return generateMarkdown(answers);
+    })
+    .then((userInput) => {
+      return writeFile(userInput);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
 
 // Function call to initialize app
 init();
---
+++ sample-README.md
@@ -0,0 +1,33 @@
+## 'README This Batman'
+
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]((https://opensource.org/licenses/MIT))
+
+  ## Description
+  'README This Batman' is a README generator.
+
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributions](#contributions)
+  * [Tests](#tests)
+  * [Questions](#questions)
+
+  ## Installation
+  npm i
+
+  ## Usage
+  Once installed, use the 'node index' command in your terminal to initialize the app.
+
+  ## License 
+  This project is licensed by MIT. Click the license badge above to learn more.
+
+  ## Contributions
+  This repo was initially built with starter code provided by the University of Oregon's Full-Stack Bootcamp.
+
+  ## Tests
+  npm test
+
+  ## Questions
+  If you have any questions about this repository, please email 
+  rhodemc@gmail.com or follow https://github.com/rhodemc.
